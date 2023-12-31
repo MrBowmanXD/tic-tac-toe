f8338805d22a6434c603ce6a54f4ac40d83f6fbc
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2023-12-31 16:21:34 +0000

    Functional game

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -27,6 +27,7 @@ class Player
   def player_nature
     if @player_two == 'c'
       puts "#{@name} vs. computer"
+      @name2 = 'computer'
       puts 'This will be fun'
     elsif @player_two == 'p'
       player_name(@player_two)
@@ -38,14 +39,27 @@ class Player
 end
 
 class GameBoard
-  def board
-    board = [[1,2,3], [4,5,6], [7,8,9]]
-    puts "| #{board[0][0]} | #{board[0][1]} | #{board[0][2]} |"
+  def initial_board
+    @board = [0,1,2,3,4,5,6,7,8]
+  end
+
+  def print_board
+    puts "| #{@board[0]} | #{@board[1]} | #{@board[2]} |"
     puts '----|---|---|'
-    puts "| #{board[1][0]} | #{board[1][1]} | #{board[1][2]} |"
+    puts "| #{@board[3]} | #{@board[4]} | #{@board[5]} |"
     puts '----|---|---|'
-    puts "| #{board[2][0]} | #{board[2][1]} | #{board[2][2]} |"
+    puts "| #{@board[6]} | #{@board[7]} | #{@board[8]} |"
     puts '----|---|---|'
+  end
+
+  def change_board(substitute, player_position)
+    if @board[substitute.to_i] == substitute.to_i && player_position == 'player_one'
+      @board[substitute.to_i] = 'X'
+    end
+
+    if @board[substitute.to_i] == substitute.to_i && player_position == 'player_two'
+      @board[substitute.to_i] = 'O'
+    end
   end
 end
 
@@ -55,13 +69,62 @@ class Game < GameBoard
     start
   end
 
+  def select_position
+    position = gets.chomp
+    position
+  end
+
+  def play(nature)
+    puts 'Enter the position you wish to place your mark'
+    position = select_position
+    if position.to_i > 8 || position.to_i < 0
+      select_position
+    end
+    change_board(position, nature)
+    print_board
+  end
+
   def start
     system('clear')
     puts 'Welcome to a new game of tic tac toe in the terminal!'
     player = Player.new
     @player = player
-    board
+    initial_board
+    print_board
+
+    play('player_one')
+    play('player_two')
+    play('player_one')
+    play('player_two')
+    play('player_one')
+    check_win('X', 'player_one')
+    play('player_two')
+    check_win('O', 'player_two')
+    play('player_one')
+    check_win('X', 'player_one')
+    play('player_two')
+    check_win('O', 'player_two')
+    play('player_one')
+    check_win('X', 'player_one')
+    play('player_two')
+    check_win('O', 'player_two')
   end
+
+  def check_win(mark, nature)
+    if (@board[0] == mark && @board[1] == mark && @board[2] == mark) ||
+      (@board[3] == mark && @board[4] == mark && @board[5] == mark) ||
+      (@board[6] == mark && @board[7] == mark && @board[8] == mark) ||
+      (@board[0] == mark && @board[3] == mark && @board[6] == mark) ||
+      (@board[1] == mark && @board[4] == mark && @board[7] == mark) ||
+      (@board[2] == mark && @board[5] == mark && @board[8] == mark) ||
+      (@board[0] == mark && @board[4] == mark && @board[8] == mark) ||
+      (@board[2] == mark && @board[4] == mark && @board[6] == mark)
+      system('clear')
+      puts "#{nature} wins, good job!"
+    end
+  end
+
+
 end
 
 Game.new
